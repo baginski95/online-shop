51cb01f05043e159e913441762303281d1fc989d
baginski95 <bagiczny3@gmail.com>
2020-12-28 20:27:02 +0100

    Created Interface for categories and products repo and implemented it with mock repositories

---
+++ OnlineShop/OnlineShop/Models/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> AllProduct { get; }
+        IEnumerable<Product> ProductsOfTheWeek { get; }
+        Product GetPieById(int productId);
+    }
+}
---
+++ OnlineShop/OnlineShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+           new List<Category>
+           {
+                new Category{CategoryId=1, CategoryName="Premium products", Description="Only for lords"},
+                new Category{CategoryId=2, CategoryName="Regular products", Description="For regular ppl"},
+                new Category{CategoryId=3, CategoryName="Good price Products", Description="100% satisfaction"}
+           };
+    }
+}
