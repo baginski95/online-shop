a2e8e1b24e873dc10d42b81d49b97583b690e006
baginski95 <bagiczny3@gmail.com>
2020-12-31 23:46:04 +0100

    Added Controller and needed Views for Home page : Added Views: - Index -Carousel(Used Bootstrap)

---
+++ OnlineShop/OnlineShop/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepoitory;
+        public HomeController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepoitory = productRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                ProductsOfTheWeek = _productRepoitory.ProductsOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
