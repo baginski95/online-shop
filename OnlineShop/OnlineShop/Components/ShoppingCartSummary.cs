7e4ca1c855af273708fcbb1e3cea90b9b067879c
baginski95 <bagiczny3@gmail.com>
2021-01-01 18:06:27 +0100

    Added new COmponent for ShoppingCart

---
+++ OnlineShop/OnlineShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart ;
+        }
+    }
+}
